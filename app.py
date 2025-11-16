4e65696f222187d6f2a2e28020240790e87bcdcc
melanycpm1 <melanycatalanpabon@gmail.com>
2025-11-16 01:36:47 -0300

    definiendo rutas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask;
+from flask import Flask,render_template,session,url_for, redirect;
 from base_de_datos.mi_conexion import Connexion
 import os
 
@@ -26,3 +26,53 @@ else:
     # Si ya existe, simplemente conecta
     db = Connexion(DB_NAME)
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("panel_principal")
+def panel_principal():
+    if "rol" not in session:
+        return redirect(url_for("login"))  # si no está logueado
+    
+    return #agregar tamblate
+
+@app.route("/gestion_inventario")
+def gestion_inventario():
+    if "rol" not in session:
+        return redirect(url_for("login"))  # si no está logueado
+    return #agregar tamblate
+
+@app.route("/gestion_pedidos")
+def gestion_pedidos():
+    if "rol" not in session:
+        return redirect(url_for("login"))  # si no está logueado
+    return  #agregar tamblate
+    
+@app.route("/gestion_usuarios")
+def gestion_usuario():
+    if "rol" not in session:
+        return redirect(url_for("login"))  # si no está logueado
+    
+    return #agregar tamblate
+
+@app.route("/reportes")
+def reportes():
+    if "rol" not in session:
+        return redirect(url_for("login"))  # si no está logueado
+
+    return  #agregar tamblate
+
+#@app.route("/notificaciones")
+#def notificaciones():
+    #  if "rol" not in session:
+    #     return redirect(url_for("login"))  # si no está logueado
+        
+    # return render_template
+@app.route("/venta")
+def venta():
+
+    if "rol" not in session:
+        return redirect(url_for("login"))  # si no está logueado
+
+    return  #agregar tamblate
